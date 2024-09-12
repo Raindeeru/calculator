8fe61189f129a3ac161291b38018090f86297135
Rin1803 <karrinfrida@gmail.com>
2024-09-12 22:36:05 +0800

    Numbers

--- src/main/java/com/example/CalculatorController.java
+++ src/main/java/com/example/CalculatorController.java
@@ -39,65 +39,116 @@ public class CalculatorController {
     @FXML Button calcPadDelete;
     @FXML Button calcPadCE;
 
+    Boolean startInput = false; 
+
     @FXML
     private void handleCalcPad1() {
         //button must input a 1
-        calcDisplay.setText("handleCalcPad1");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+1);
+        else{ 
+            calcDisplay.setText("1");
+            startInput = true; 
+        }
+        //calcDisplay.setText("handleCalcPad1");
     }
 
-    
     @FXML
     private void handleCalcPad2() {
         //button must input a 2
-        calcDisplay.setText("handleCalcPad2");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+2);
+        else{ 
+            calcDisplay.setText("2");
+            startInput = true; 
+        }
+        //calcDisplay.setText("handleCalcPad2");
     }
 
     @FXML
     private void handleCalcPad3() {
         //button must input a 3
-        calcDisplay.setText("handleCalcPad3");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+3);
+        else{ 
+            calcDisplay.setText("3");
+            startInput = true; 
+        }
+        //calcDisplay.setText("handleCalcPad3");
     }
 
     @FXML
     private void handleCalcPad4() {
         //button must input a 4
-        calcDisplay.setText("handleCalcPad4");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+4);
+        else{ 
+            calcDisplay.setText("4");
+            startInput = true; 
+        }
+        //calcDisplay.setText("handleCalcPad4");
     }
 
     @FXML
     private void handleCalcPad5() {
         //button must input a 5
-        calcDisplay.setText("handleCalcPad5");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+5);
+        else{ 
+            calcDisplay.setText("5");
+            startInput = true; 
+        }
+        //calcDisplay.setText("handleCalcPad5");
     }
 
     @FXML
     private void handleCalcPad6() {
         //button must input a 6
-        calcDisplay.setText("handleCalcPad6");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+6);
+        else{ 
+            calcDisplay.setText("6");
+            startInput = true; 
+        }
+        //calcDisplay.setText("handleCalcPad6");
     }
 
     @FXML
     private void handleCalcPad7() {
         //button must input a 7
-        calcDisplay.setText("handleCalcPad7");
+        //calcDisplay.setText("handleCalcPad7");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+7);
+        else{ 
+            calcDisplay.setText("7");
+            startInput = true; 
+        }
     }
 
     @FXML
     private void handleCalcPad8() {
         //button must input a 8
-        calcDisplay.setText("handleCalcPad8");
+        //calcDisplay.setText("handleCalcPad8");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+8);
+        else{ 
+            calcDisplay.setText("8");
+            startInput = true; 
+        }
     }
 
     @FXML
     private void handleCalcPad9() {
         //button must input a 9
-        calcDisplay.setText("handleCalcPad9");
+        //calcDisplay.setText("handleCalcPad9");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+9);
+        else{ 
+            calcDisplay.setText("9");
+            startInput = true; 
+        }
     }
 
     @FXML
     private void handleCalcPad0() {
         //button must input a 0
-        calcDisplay.setText("0");
+        //calcDisplay.setText("0");
+        if(startInput) calcDisplay.setText(calcDisplay.getText()+0);
+        else{ 
+            calcDisplay.setText("0");
+            startInput = true; 
+        }
     }
 
     @FXML
