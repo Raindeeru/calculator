540dc32679a8110f6bc72bb0ba7fd9fe74cd3b0d
Raindeeru <derv.gonzales17@gmail.com>
2024-09-12 17:04:24 +0800

    nag add ako ng group name member tas kung ano man

--- src/main/java/com/example/CalculatorController.java
+++ src/main/java/com/example/CalculatorController.java
@@ -4,9 +4,15 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 /**
- * Group Name: <Group Name>
- * Group Members: <Group Members>
- * Course/Section: <Course/Section>
+ * Group Name: Guo x Quiboloy
+ * Group Members:   Matthew Payor
+ *                  Ricardo Gayagoy
+ *                  Louis Gascon
+ *                  Derven Gonzales
+ *                  John Joshua Botabara
+ *                  Karrin Frida Novero
+ *                  Bryle Florentino
+ * Course/Section:  CSS124L/BM1
  */
 
 public class CalculatorController {
